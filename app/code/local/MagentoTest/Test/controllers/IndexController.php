a303dad0d90a7d9f62d6da935619058d19c9d53c
Zsolt Gál <technodelight@gmail.com>
2012-09-19 00:59:06 +0200

    - Create a new module, which will defines a new route and controller. It should listen to the following url: http://magento.development.local/test and it should display the following text: Today is a happy day.

---
+++ app/code/local/MagentoTest/Test/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * IndexController
+ * 
+ * Controller for displaying various test data
+ * @package magento.development.local
+ * @since 2012.09.17.
+ * @author Zsolt Gál
+ */
+class MagentoTest_Test_IndexController extends Mage_Core_Controller_Front_Action {
+
+	public function indexAction() {
+		echo "Today is a happy day";
+	}
+
+}
