9fe2462e3a5fa81d2643d6e4034a055fe46a6577
Zsolt Gál <technodelight@gmail.com>
2012-09-19 03:02:55 +0200

    - Create a new template, which will contain the static text from the previous sentence and remove it from the controller. Please use the translator method, so your text can be translated to any languages.

--- app/code/local/MagentoTest/Test/controllers/IndexController.php
+++ app/code/local/MagentoTest/Test/controllers/IndexController.php
@@ -11,7 +11,8 @@
 class MagentoTest_Test_IndexController extends Mage_Core_Controller_Front_Action {
 
 	public function indexAction() {
-		echo "Today is a happy day";
+		$this->loadLayout();
+		$this->renderLayout();
 	}
 
 }
